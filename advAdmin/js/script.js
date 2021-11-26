32f55fa0e0369b74eab80ecbbff1f3a2d153067d
piton <piton1808@gmai.com>
2021-11-26 18:19:43 +0700

    отобраение информации об объекте и слайдер для фото

--- advAdmin/js/script.js
+++ advAdmin/js/script.js
@@ -32,17 +32,19 @@ class App {
     this.container = document.querySelector('.main');
     this.currentItem = '';
     this.currentElem = '';
+    this.docsFiles = [];
+    this.photoFiles = [];
     this.timerUpdateItems = setInterval(() => {
       this.getNewItems();
     }, 300000)
   }
   init(){
-    this.getItem(this.items[0].reqNumber);
-    // todo удалить снизу костыль
-    // this.getItem(57424000046);
     this.container.insertAdjacentHTML('beforeend', this.layout());
     this.currentElem = document.querySelector('.list__item');
     this.currentElem.classList.add('list__item_active');
+    this.getItem(this.items[0].reqNumber);
+    // todo удалить снизу костыль
+    // this.getItem(57424000046);
     this.handler();
   }
 
@@ -81,6 +83,104 @@ class App {
             <div class="footer"></div>`
   }
 
+  renderItem(){
+    const centerField = document.querySelector('.center-side');
+    centerField.innerHTML = '';
+    if (this.currentItem){
+      centerField.insertAdjacentHTML('beforeend', this.centerLayout())
+      this.checkSlider();
+    } else {
+      centerField.insertAdjacentHTML('beforeend',`<p class="center-side__empty">Нет данных по объекту</p>`);
+    }
+  }
+  sortFilesItem(){
+    this.docsFiles = [];
+    this.photoFiles = [];
+    for (let file of this.currentItem.files){
+      if (file.isDoc){
+        this.docsFiles.push(file);
+      } else {
+        this.photoFiles.push(file);
+      }
+    }
+  }
+  getPhotoItem(files){
+    if (files.length > 0){
+      const regExp = new RegExp('pdf', 'i');
+      const placeholderPDF = 'https://crm.centralnoe.ru/advertisement/img/default/pdf.png';
+      let photos = {
+        photoLayout: '',
+        startPhoto: regExp.test(files[0].url) ? placeholderPDF : files[0].url
+      };
+      for (let photo of files){
+        photos.photoLayout += `<div class="slider__item slider__photo" data-img=${photo.url} style="background-image: url(${regExp.test(photo.url) ? placeholderPDF : photo.url})"></div>`
+      }
+      return photos;
+    } else {
+      return ''
+    }
+  }
+  checkSlider(){
+    const elms = document.querySelectorAll('.slider');
+    for (let i = 0, len = elms.length; i < len; i++) {
+      // инициализация elms[i] в качестве слайдера
+      new ChiefSlider(elms[i]);
+    }
+  }
+  centerLayout(){
+    const photo = this.getPhotoItem(this.docsFiles.length > 0 ? this.docsFiles : this.photoFiles);
+    return ` <span class="card__title">
+                ${this.currentItem.object.city ? `г. ${this.currentItem.object.city}` : ''} 
+                ${this.currentItem.object.street ? `ул.${this.currentItem.object.street}` : ''} 
+                ${this.currentItem.object.houseNumber ? `д. ${this.currentItem.object.houseNumber}` : ''} 
+                ${this.currentItem.objectRoom ? `кв. ${this.currentItem.objectRoom}` : ''} 
+              </span>
+            <div class="center-side__top"> 
+              <div class="card">
+                <div class="card__info"> 
+                  <p class="card__info-text">Заявка:<span>${this.currentItem.ad ? this.currentItem.ad : ''} </span></p>
+                  <p class="card__info-text">Клиент:<span>${this.currentItem.clients[0] ? 
+                    `${this.currentItem.clients[0].secondName ? this.currentItem.clients[0].secondName : ''}
+                    ${this.currentItem.clients[0].name ? this.currentItem.clients[0].name : ''}
+                    ${this.currentItem.clients[0].lastName ? this.currentItem.clients[0].lastName : ''}`
+                    : ''} </span>
+                  </p>
+                  <p class="card__info-text">Тип отношений:<span>${this.currentItem.type.type ? this.currentItem.type.type : ''} </span></p>
+                  <p class="card__info-text">Срок действия:<span>${this.currentItem.publishedAt.stop ? this.currentItem.publishedAt.stop : ''} </span></p>
+                  <p class="card__info-text">Тип объекста:<span>${this.currentItem.objectType ? 
+                    `${this.currentItem.objectType.type ? this.currentItem.objectType.type : ''}
+                    ${this.currentItem.objectType.rooms ? `(${this.currentItem.objectType.rooms}к.)` : ''}`
+                    : ''} </span></p>
+                  <p class="card__info-text">Комната объекта:<span>${this.currentItem.objectRoom ? this.currentItem.objectRoom : ''} </span></p>
+                  <p class="card__info-text">Доля объекта:<span>${this.currentItem.objectShare ? this.currentItem.objectShare : ''} </span></p>
+                </div> 
+                <div class="card__comment"> 
+                  <span class="card__comment-title">Комментарий в рекламу</span>
+                  <textarea class="card__comment-field input" cols="30" rows="5">${this.currentItem.comment ? this.currentItem.comment : ''}</textarea>
+                </div>
+              </div>
+              <div class="photo"> 
+                <img class="photo__img" src="${photo.startPhoto}" alt="photo">   
+              </div>                
+            </div>
+            <div class="center-side__bottom"> 
+              <div class="carousel"> 
+                <div class="slider">
+                  <div class="slider__container">
+                      <div class="slider__wrapper">
+                          <div class="slider__items">
+                             ${photo.photoLayout}
+                          </div>
+                      </div>
+                  </div>
+                <!-- Кнопки для перехода к предыдущему и следующему слайду -->
+                <a href="#" class="slider__control" data-slide="prev"></a>
+                <a href="#" class="slider__control" data-slide="next"></a>
+                </div>
+              </div>
+            </div>`
+  }
+
   handler(){
     this.container.addEventListener('click', event => {
       if (event.target.dataset.item){
@@ -100,9 +200,16 @@ class App {
       reqNumber: reqNumber,
     }).then(item => {
       //todo отрисовывать объект в центре
-      this.currentItem = item;
-      console.log('this is item')
-      console.log(item)
+      if(!Array.isArray(item)){
+        this.currentItem = item;
+        console.log('this is item')
+        console.log(this.currentItem)
+        this.sortFilesItem();
+        this.renderItem();
+      } else {
+        this.currentItem = '';
+        this.renderItem();
+      }
     })
   }
   getNewItems(){
