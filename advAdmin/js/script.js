0f072d316952fce40be499ab176e6234ea2127de
piton <piton1808@gmai.com>
2021-11-26 10:05:26 +0700

    добавили функционал для модератора

---
+++ advAdmin/js/script.js
@@ -0,0 +1,130 @@
+class Api {
+  constructor() {
+    this.API = 'https://50970.vds.miran.ru:553/Servers/Internal/AdAdmin.php';
+  }
+  async getJson(requestNamed){
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json; charset=utf-8");
+    const raw = JSON.stringify(requestNamed);
+    const requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: "include",
+      headers: myHeaders,
+      body: raw
+    };
+
+    let response = await fetch(this.API, requestOptions);
+    if (!response.ok) {
+      throw new Error('Ответ сети был не ok.');
+    }
+
+    let jsonA = await response.json();
+    return jsonA;
+  }
+}
+
+class App {
+  constructor(data) {
+    this.items = data.data;
+    this.serverTime = data.serverTime;
+    this.container = document.querySelector('.main');
+    this.currentItem = data.data[0];
+    this.currentElem = '';
+    this.timerUpdateItems = setInterval(() => {
+      // this.getNewItems();
+    }, 3000)
+  }
+  init(){
+    // this.getItem(this.currentItem.reqNumber);
+    // todo удалить снизу костыль
+    this.getItem(57424000046);
+    this.container.insertAdjacentHTML('beforeend', this.layout());
+    this.currentElem = document.querySelector('.list__item');
+    this.currentElem.classList.add('list__item_active');
+    this.handler();
+  }
+  getStatus(item){
+    return 'btn_status_approved'
+  }
+  getList(){
+
+    let listLayout = '';
+    for (let item of this.items){
+      listLayout += `<div class="list__item" data-item="${item.reqNumber}"> 
+                      <div class="list__status"> 
+                        <span class="btn_status ${this.getStatus(item)}"></span>
+                      </div>
+                      <div class="list__text_wrap"> 
+                        <span class="list__text list__req">${item.reqNumber}</span>
+                        <span class="list__text list__location">${item.reqStreet} ${item.reqHouseNumber}</span>
+                      </div>
+                      <span class="list__text list__type">${item.reqType}</span>
+                     </div>`
+    }
+    return listLayout;
+  }
+  layout(){
+    const list = this.getList();
+    return `<div class="header"></div>
+              <div class="left-side">
+              <div class="left-side__input"> 
+                <input class="input input__search" type="search" placeholder="поиск">
+<!--                <div></div>-->
+              </div> 
+              <div class="list"> 
+                ${list}
+              </div>
+            </div>
+            <div class="center-side"> 
+            </div>
+            <div class="right-side"></div>
+            <div class="footer"></div>`
+  }
+
+  handler(){
+    this.container.addEventListener('click', event => {
+      if (event.target.dataset.item){
+        this.toggleActive(event.target);
+        this.getItem(event.target.dataset.item);
+      }
+    })
+  }
+  toggleActive(newElem){
+    this.currentElem.classList.remove('list__item_active');
+    this.currentElem = newElem;
+    this.currentElem.classList.add('list__item_active');
+  }
+  getItem(reqNumber){
+    api.getJson({
+      action: 'getItem',
+      reqNumber: reqNumber,
+    }).then(item => {
+      //todo отрисовывать объект в центре
+      console.log(item)
+    })
+  }
+  getNewItems(){
+    api.getJson({
+      action: 'getUpdates',
+      serverTime: this.serverTime,
+    }).then(newData => {
+      this.serverTime = newData.serverTime;
+      if (+newData.items > 0){
+      //todo отрисовать и запушить newData.data
+      }
+      console.log(newData);
+    })
+  }
+}
+
+const api = new Api();
+let app = '';
+api.getJson({
+  action : "getList"
+}).then(data => {
+  console.log(data)
+  app = new App(data);
+  app.init();
+})
